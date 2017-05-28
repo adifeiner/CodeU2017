ea5fe0aefe2597d9d9439cb3d580a26a260eb981
adifeiner <adifeiner@users.noreply.github.com>
2017-05-28 14:47:11 +0300

    Changed the code according to the code review
    1. Removed the documentation comments from line 3
    2. Added spaces and blank lines
    3. Used Map.getOrDefault in the method isPermutation
    4. Moved class definitions to the start

--- Ex1.java
+++ Ex1.java
@@ -1,82 +1,48 @@
 import java.util.HashMap;
 
+class Node {
+
+    private int data;
+    private Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData(){
+        return this.data;
+    }
+
+    public Node getNext(){
+        return this.next;
+    }
+
+    public void setNext(Node next){
+        this.next = next;
+    }
+}
+
+class LinkedList {
+
+    private Node head;
+
+    public Node getHead(){
+        return this.head;
+    }
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+
+        if (this.head != null) {
+            newNode.setNext(this.head);
+        }
+
+        this.head = newNode;
+    }
+}
+
 public class Ex1 {
-
-    /**
-     * The function receives two strings and checks if one is a permutation
-     * of the other
-     * @param firstStr
-     * @param secondStr
-     * @return true if one is a permutation of the other, otherwise return false
-     */
-    public static boolean isPermutation (String firstStr, String secondStr){
-        // Check if one of the strings are null or if their length is not equal
-        if (firstStr == null || secondStr == null || firstStr.length() != secondStr.length()){
-            return false;
-        }
-
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-
-        // Create a hash map so that the key is the character
-        // and the value is the number of times that the character appears in the string
-        for(int i = 0; i < firstStr.length(); i++){
-            Character currChar = Character.toLowerCase(firstStr.charAt(i));
-            // Check if the map contains a mapping for the specified character
-            if (map.containsKey(currChar)){
-                // Update the value of the character by increasing it by one
-                map.put(currChar, map.get(currChar) + 1);
-            } else {
-                // Create a new key with value one
-                map.put(currChar, 1);
-            }
-        }
-
-        // Check if the second string is a permutation of the first string
-        for (int i = 0; i < secondStr.length(); i++){
-            Character currChar = Character.toLowerCase(secondStr.charAt(i));
-            // Check if the map contains a mapping for the specified character
-            // and if its value is greater than zero
-            if (map.containsKey(currChar) && map.get(currChar) > 0){
-                // Update the value of the character by reducing it by one
-                map.put(currChar, map.get(currChar) - 1);
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * The function finds the kth to last element of a singly linked list
-     * @param list
-     * @param k
-     * @return The data of the node kth to last element
-     */
-    public static int findKthToLastElement(LinkedList list, int k){
-        // If the list is empty, return -1
-        if(list.getHead() == null){
-            return -1;
-        }
-
-        // The first pointer progress k steps from the head node of the list
-        Node firstPointer = list.getHead();
-        for(int i = 0; i < k; i++){
-            firstPointer = firstPointer.getNext();
-        }
-
-        Node secondPointer = list.getHead();
-
-        // As long as the next node of the first pointer isn't null, the first pointer and the second pointer
-        // progress to the next node
-        while (firstPointer.getNext() != null){
-            firstPointer = firstPointer.getNext();
-            secondPointer = secondPointer.getNext();
-        }
-        // When the first pointer is at the last node of the list,
-        // the second pointer is on kth to last element of the list
-        return secondPointer.getData();
-    }
-
 
     public static void main(String[] args) {
         //test q1
@@ -91,44 +57,74 @@ public class Ex1 {
         list.addFirst(4);
         System.out.println(findKthToLastElement(list, 2)); // 2
     }
+
+    public static boolean isPermutation(String firstStr, String secondStr) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Check if one of the strings are null or if their length is not equal
+        if (firstStr == null || secondStr == null || firstStr.length() != secondStr.length()) {
+            return false;
+        }
+
+        // Create a hash map so that the key is the character
+        // and the value is the number of times that the character appears in the string
+        for (int i = 0; i < firstStr.length(); i++) {
+            Character currChar = Character.toLowerCase(firstStr.charAt(i));
+
+            // Update the value of the character by increasing it by one or create a new key with value one
+            map.put(currChar, map.getOrDefault(currChar, 0) + 1);
+        }
+
+        // Check if the second string is a permutation of the first string
+        for (int i = 0; i < secondStr.length(); i++) {
+            Character currChar = Character.toLowerCase(secondStr.charAt(i));
+
+            // Save the value of the currChar if mapped, otherwise zero
+            int num = map.getOrDefault(currChar, 0);
+
+            // If num is greater than zero, update the value of the character by reducing it by one
+            if (num > 0) {
+                map.put(currChar, num - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * The function finds the kth to last element of a singly linked list
+     * @param list
+     * @param k
+     * @return The data of the node kth to last element
+     */
+    public static int findKthToLastElement(LinkedList list, int k) {
+        // If the list is empty, return -1
+        if (list.getHead() == null) {
+            return -1;
+        }
+
+        // The first pointer progress k steps from the head node of the list
+        Node firstPointer = list.getHead();
+
+        for (int i = 0; i < k; i++) {
+            firstPointer = firstPointer.getNext();
+        }
+        Node secondPointer = list.getHead();
+
+        // As long as the next node of the first pointer isn't null, the first pointer and the second pointer
+        // progress to the next node
+        while (firstPointer.getNext() != null) {
+            firstPointer = firstPointer.getNext();
+            secondPointer = secondPointer.getNext();
+        }
+        // When the first pointer is at the last node of the list,
+        // the second pointer is on kth to last element of the list
+        return secondPointer.getData();
+    }
 }
 
-    class Node{
-        private int data;
-        private Node next;
 
-        public Node(int data){
-            this.data = data;
-            this.next = null;
-        }
 
-        public int getData(){
-            return this.data;
-        }
-
-        public Node getNext(){
-            return this.next;
-        }
-
-        public void setNext(Node next){
-            this.next = next;
-        }
-    }
-
-    class LinkedList{
-        private Node head;
-
-        public Node getHead(){
-            return this.head;
-        }
-
-        public void addFirst(int data){
-            Node newNode = new Node(data);
-            if (this.head != null) {
-                newNode.setNext(this.head);
-            }
-            this.head = newNode;
-        }
-    }
 
 
