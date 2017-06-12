4a6b5c3d22f7bcb483c02c339ad3c1210db8db44
adifeiner <adifeiner@users.noreply.github.com>
2017-06-12 20:00:28 +0300

    Updated BinaryTreeNode
    Added the methods findNode and containsKey

--- BinaryTreeNode.java
+++ BinaryTreeNode.java
@@ -1,32 +1,62 @@
 public class BinaryTreeNode<T> {
-        private final T data;
-        private BinaryTreeNode<T> leftChild;
-        private BinaryTreeNode<T> rightChild;
+    private final T data;
+    private BinaryTreeNode<T> leftChild;
+    private BinaryTreeNode<T> rightChild;
 
-        public BinaryTreeNode(T data) {
-            this.data = data;
-            this.leftChild = null;
-            this.rightChild = null;
+    public BinaryTreeNode(T data) {
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    public BinaryTreeNode findNode(T key) {
+        BinaryTreeNode<T> node = null;
+
+        if (this == null) {
+            return null;
+        }
+        if (this.data.equals(key)) {
+            return this;
         }
 
-        public T getData() {
-            return this.data;
+        if (this.getLeftChild() != null) {
+            node = this.getLeftChild().findNode(key);
+            if (node != null) {
+                return node;
+            }
         }
 
-        public BinaryTreeNode<T> getLeftChild() {
-            return this.leftChild;
+        if (this.getRightChild() != null) {
+            node = this.getRightChild().findNode(key);
         }
+        return node;
+    }
 
-        public BinaryTreeNode<T> getRightChild() {
-            return this.rightChild;
+    public boolean containsKey(T key) {
+        if (this.findNode(key) != null) {
+            return true;
         }
+        return false;
+    }
 
-        public void setLeftChild(T data) {
-            this.leftChild = new BinaryTreeNode(data);
-        }
+    public T getData() {
+        return this.data;
+    }
 
-        public void setRightChild(T data) {
-            this.rightChild = new BinaryTreeNode(data);
-        }
+    public BinaryTreeNode<T> getLeftChild() {
+        return this.leftChild;
+    }
+
+    public BinaryTreeNode<T> getRightChild() {
+        return this.rightChild;
+    }
+
+    public void setLeftChild(T data) {
+        this.leftChild = new BinaryTreeNode(data);
+    }
+
+    public void setRightChild(T data) {
+        this.rightChild = new BinaryTreeNode(data);
+    }
 }
 
