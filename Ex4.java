e8697b41f1380a532e66532def79d6b039f3e108
adifeiner <adifeiner@users.noreply.github.com>
2017-07-01 18:02:17 +0300

    Updating Ex4 and Ex4Test

--- Ex4.java
+++ Ex4.java
@@ -2,10 +2,11 @@ public class Ex4 {
 
     /**
      * The fuction loop over the mapTiles array and count the number of islands
-     * @param numOfRow
-     * @param numOfCol
+     *
+     * @param numOfRow - the number of rows
+     * @param numOfCol - the number of columns
      * @param mapTiles - 2-dimensional array of booleans where false means water and true means land
-     * @return
+     * @return - the number of islands
      */
     public static int countIslands(int numOfRow, int numOfCol, boolean[][] mapTiles) {
         int numOfIslands = 0;
@@ -15,7 +16,7 @@ public class Ex4 {
                 if (!visit[i][j] && mapTiles[i][j]) {
                     numOfIslands++;
                     // find all the tiles that belong to this island
-                    findAllTilesOfIsland(mapTiles, visit, numOfRow, numOfCol, i, j);
+                    markAllTilesOfIsland(mapTiles, visit, numOfRow, numOfCol, i, j);
                 }
             }
         }
@@ -23,25 +24,26 @@ public class Ex4 {
     }
 
     /**
-     * The function check if the arguments legal and check
-     * all the options for tiles belong to the same island
-     * @param mapTiles - 2-dimensional array of booleans where false means water and true means land
+     * The function finding all tiles that belong to the island containing tile (rowIndex, colIndex)
+     * and marking them as visited.
+     *
+     *  @param mapTiles - 2-dimensional array of booleans where false means water and true means land
      * @param visit - 2-dimensional array of booleans
      * @param numOfRows - number of rows
      * @param numOfCols - number of columns
      * @param rowIndex - row index in the tiles array
      * @param colIndex - column index in te tiles array
      */
-    private static void findAllTilesOfIsland(boolean[][] mapTiles, boolean[][] visit, int numOfRows, int numOfCols,
+    private static void markAllTilesOfIsland(boolean[][] mapTiles, boolean[][] visit, int numOfRows, int numOfCols,
                                        int rowIndex, int colIndex) {
         if (rowIndex < 0 || rowIndex >= numOfRows || colIndex < 0 || colIndex >= numOfCols || !mapTiles[rowIndex][colIndex]
                 || visit[rowIndex][colIndex]) {
             return;
         }
         visit[rowIndex][colIndex] = true;
-        findAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex - 1, colIndex);
-        findAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex + 1, colIndex);
-        findAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex, colIndex - 1);
-        findAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex, colIndex + 1);
+        markAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex - 1, colIndex);
+        markAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex + 1, colIndex);
+        markAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex, colIndex - 1);
+        markAllTilesOfIsland(mapTiles, visit, numOfRows, numOfCols, rowIndex, colIndex + 1);
     }
 }
--- Ex4Test.java
+++ Ex4Test.java
@@ -6,34 +6,30 @@ public class Ex4Test {
 
     @Test
     public void testCountIslands(){
-        boolean[][] mapTiles = new boolean[4][4];
-        mapTiles[0][1] = true;
-        mapTiles[1][0] = true;
-        mapTiles[1][1] = true;
-        mapTiles[0][3] = true;
-        mapTiles[2][2] = true;
-        mapTiles[3][2] = true;
-        Ex4 countIslands = new Ex4();
-        assertEquals(3, countIslands.countIslands(mapTiles.length, mapTiles[0].length, mapTiles));
+        boolean[][] mapTiles = {
+                {false, true, false, true},
+                {true, true, false, false},
+                {false, false, true, false},
+                {false, false, true, false}
+        };
+        assertEquals(3, Ex4.countIslands(mapTiles.length, mapTiles[0].length, mapTiles));
     }
 
     @Test
     public void testZeroIsland(){
         boolean[][] mapTiles = new boolean[4][4];
-        Ex4 countIslands = new Ex4();
-        assertEquals(0, countIslands.countIslands(mapTiles.length, mapTiles[0].length, mapTiles));
+        assertEquals(0, Ex4.countIslands(mapTiles.length, mapTiles[0].length, mapTiles));
     }
 
     @Test
     public void testOneIsland(){
-        boolean[][] mapTiles = new boolean[4][4];
-        for(int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                mapTiles[i][j] = true;
-            }
-        }
-        Ex4 countIslands = new Ex4();
-        assertEquals(1, countIslands.countIslands(mapTiles.length, mapTiles[0].length, mapTiles));
+        boolean[][] mapTiles = {
+                {true, true, true, true},
+                {true, true, true, true},
+                {true, true, true, true},
+                {true, true, true, true}
+        };
+        assertEquals(1, Ex4.countIslands(mapTiles.length, mapTiles[0].length, mapTiles));
     }
 
 
